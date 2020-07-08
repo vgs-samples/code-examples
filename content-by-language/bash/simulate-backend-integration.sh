20499217655178e0635973b648294ead091fc9e9
Ruslan Malogulko <ruslan.malogulko@verygoodsecurity.com>
2020-07-08 16:34:14 +0300

    Update code snippet

--- content-by-language/bash/simulate-backend-integration.sh
+++ content-by-language/bash/simulate-backend-integration.sh
@@ -1,5 +1,5 @@
 curl https://api.acme.com -k -X POST \\
-    -x $HTTPS_PROXY_USERNAME:$HTTPS_PROXY_PASSWORD@$VAULT_ID.sandbox.verygoodproxy.com:8080 \\
+    -x __ACCESS_CREDENTIALS__@__VAULT_HOST__:8080 \\
     -X POST \\
     -H "Content-type: application/json" \\
     -d '{"ssn": "$VGS_TOKEN"}'
