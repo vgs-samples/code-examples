e6d860f9f44526aeff43bc83021b2e05eb9ecd44
Ruslan Malogulko <ruslan.malogulko@verygoodsecurity.com>
2020-07-20 15:59:27 +0300

    Update inbound integration code snippet

--- content-by-language/node/inbound-integration.js
+++ content-by-language/node/inbound-integration.js
@@ -1,17 +1,21 @@
-const request = require('request');
+const fetch = require('node-fetch');
 
-request(
-  {
-    url: '{VAULT_URL}/post',
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ secret: 'secret_value' }),
-  },
-  function(error, response, body) {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log(JSON.parse(body));
-    }
+async function getData() {
+  let result;
+
+  try {
+    result = await fetch('{VAULT_URL}/post', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        secret: 'secret_value',
+      }),
+    });
+  } catch (e) {
+    console.error(e);
   }
-);
+
+  return await result.json();
+}
+
+getData().then(response => console.log(response));
