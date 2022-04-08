2aa702457a72ab2c9884be78372ecc02e50ff18e
Evgen Korobchuk (VGS) <44065191+awaesome@users.noreply.github.com>
2022-04-08 13:34:42 +0300

    [refactor] secure-port

--- content-by-language/bash/outbound-integration.sh
+++ content-by-language/bash/outbound-integration.sh
@@ -1,4 +1,4 @@
 curl {VGS_SAMPLE_ECHO_SERVER}/post {IGNORE_SSL_FLAG} \\
-  -x {SECURE_PROTOCOL}://{ACCESS_CREDENTIALS}@{VAULT_HOST}:{PORT} \\
+  -x {SECURE_PROTOCOL}://{ACCESS_CREDENTIALS}@{VAULT_HOST}:{SECURE_PORT} \\
   -H "Content-type: application/json" \\
   -d '{"account_number": "{ALIAS}"}'
--- content-by-language/python/outbound-integration.py
+++ content-by-language/python/outbound-integration.py
@@ -1,7 +1,7 @@
 import os
 import requests
 
-os.environ['HTTPS_PROXY'] = '{SECURE_PROTOCOL}://{ACCESS_CREDENTIALS}@{VAULT_HOST}:{PORT}'
+os.environ['HTTPS_PROXY'] = '{SECURE_PROTOCOL}://{ACCESS_CREDENTIALS}@{VAULT_HOST}:{SECURE_PORT}'
 response = requests.post('{VGS_SAMPLE_ECHO_SERVER}/post',
                          json={'account_number': '{ALIAS}'},
                          verify='{CERT_LOCATION}')
