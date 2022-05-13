1601a0f17fea667609d96d8d5c7c0ab86b4e476a
Sasha Kochniev <oleksandr.kochniev@vgs.io>
2022-05-13 17:23:11 +0300

    CORE-1167 - update code sample for https proxy

--- content-by-language/java/outbound-integration.java
+++ content-by-language/java/outbound-integration.java
@@ -1,68 +1,120 @@
-// Run with flag: -Djdk.http.auth.tunneling.disabledSchemes=""
-// More info: https://www.oracle.com/java/technologies/javase/8u111-relnotes.html
-
 package com.verygoodsecurity;
 
+import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.net.ProxySelector;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.security.GeneralSecurityException;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.security.KeyManagementException;
 import java.security.KeyStore;
-import java.security.cert.Certificate;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
-import java.util.Base64;
-
+import java.security.cert.X509Certificate;
+import java.util.Arrays;
+import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.TrustManagerFactory;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContexts;
+import org.apache.http.util.EntityUtils;
 
 public class OutboundIntegration {
-  public static void main(String[] args) throws IOException, InterruptedException, GeneralSecurityException {
-    System.setProperty("jdk.http.auth.tunneling.disabledSchemes", "");
-    final String proxyHost = "{VAULT_HOST}";
-    final var proxyPort = {SECURE_PORT};
-    final String proxyUser = "{USERNAME}";
-    final String proxyPassword = "{PASSWORD}";
 
-    final HttpClient client = HttpClient.newBuilder()
-        .sslContext(buildSSLContext())
-        .proxy(ProxySelector.of(new InetSocketAddress(proxyHost, proxyPort)))
-        .build();
-    final String proxyAuthentication = proxyUser + ":" + proxyPassword;
-    final String proxyAuthenticationEncoded = new String(Base64.getEncoder().encode(proxyAuthentication.getBytes()));
-    final HttpRequest request = HttpRequest.newBuilder()
-        .uri(URI.create("{VGS_SAMPLE_ECHO_SERVER}/post"))
-        .header("Content-Type", "application/json")
-        .POST(HttpRequest.BodyPublishers.ofString("{\"account_number\":\"{ALIAS}\"}"))
-        .setHeader("Proxy-Authorization", "Basic " + proxyAuthenticationEncoded)
-        .build();
-    final HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-
-    System.out.println("status code=" + response.statusCode());
-    System.out.println("response=" + response.body());
+  public static void main(String[] args) throws IOException, CertificateException, NoSuchAlgorithmException, KeyStoreException, KeyManagementException {
+    tlsProxy();
   }
 
-  private static SSLContext buildSSLContext() throws IOException, GeneralSecurityException {
-    final KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
-    keyStore.load(null);
+  private static void tlsProxy() throws IOException, CertificateException, NoSuchAlgorithmException, KeyStoreException, KeyManagementException {
+    final int proxyPort = {SECURE_PORT};
+    CredentialsProvider credsProvider = new BasicCredentialsProvider();
+    credsProvider.setCredentials(
+        new AuthScope("{VAULT_HOST}", proxyPort),
+        new UsernamePasswordCredentials("{USERNAME}", "{PASSWORD}"));
+    HttpHost proxy = new HttpHost("{VAULT_HOST}", proxyPort, "https");
+    HttpHost target = new HttpHost("{VGS_SAMPLE_ECHO_SERVER}", 443, "https");
 
-    FileInputStream fileInputStream = new FileInputStream("{CERT_LOCATION}");
+    CloseableHttpClient httpclient = HttpClients.custom()
+        .setSSLSocketFactory(getSslConnectionSocketFactory())
+        .setDefaultCredentialsProvider(credsProvider).build();
 
-    final Certificate certificate = CertificateFactory.getInstance("X.509")
-        .generateCertificate(fileInputStream);
-    keyStore.setCertificateEntry("vgs", certificate);
+    try {
 
-    final TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
-    tmf.init(keyStore);
-    final TrustManager[] trustManagers = tmf.getTrustManagers();
+      RequestConfig config = RequestConfig.custom()
+          .setProxy(proxy)
+          .build();
+      HttpPost httpPost = getRequest(config);
 
-    final SSLContext sslContext = SSLContext.getInstance("TLS");
-    sslContext.init(null, trustManagers, null);
-    return sslContext;
+
+      CloseableHttpResponse response = httpclient.execute(target, httpPost);
+      try {
+        System.out.println("status code=" + response.getStatusLine());
+        System.out.println("response=" + EntityUtils.toString(response.getEntity()));
+      } finally {
+        response.close();
+      }
+    } catch (ClientProtocolException e) {
+      e.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
+    } finally {
+      httpclient.close();
+    }
+  }
+
+  private static HttpPost getRequest(RequestConfig config) throws UnsupportedEncodingException {
+    HttpPost httpPost = new HttpPost("/post");
+    HttpEntity requestEntity = new StringEntity("{\"account_number\":\"{ALIAS}\"}");
+    httpPost.setHeader("Content-Type", "application/json");
+    httpPost.setEntity(requestEntity);
+    httpPost.setConfig(config);
+    return httpPost;
+  }
+
+  private static SSLConnectionSocketFactory getSslConnectionSocketFactory() throws CertificateException, NoSuchAlgorithmException, KeyStoreException, IOException, KeyManagementException {
+
+    SSLContext sslcontext = SSLContexts.custom()
+        .loadTrustMaterial(getKeystore(), null)
+        .build();
+    return new SSLConnectionSocketFactory(
+        sslcontext,
+        new String[] { "TLSv1.2" },
+        null,
+        SSLConnectionSocketFactory.getDefaultHostnameVerifier());
+
+  }
+
+  private static KeyStore getKeystore() throws KeyStoreException, CertificateException, IOException, NoSuchAlgorithmException {
+    KeyStore ks = KeyStore.getInstance(KeyStore.getDefaultType());
+    ks.load(null, "vgs".toCharArray());
+
+    FileInputStream fis = new FileInputStream("{CERT_LOCATION}");
+    X509Certificate vgsSelfSigned = (X509Certificate) CertificateFactory.getInstance("X.509")
+        .generateCertificate(new BufferedInputStream(fis));
+    ks.setCertificateEntry("VGS Self Signed Certificate", vgsSelfSigned);
+    URL destinationURL = new URL("https://" + "{VAULT_HOST}");
+    HttpsURLConnection conn = (HttpsURLConnection) destinationURL.openConnection();
+    conn.connect();
+    Arrays.stream(conn.getServerCertificates()).forEach(certificate -> {
+      try {
+        ks.setCertificateEntry(String.valueOf(certificate.hashCode()), certificate);
+      } catch (KeyStoreException e) {
+        e.printStackTrace();
+      }
+    });
+    return ks;
   }
 }
