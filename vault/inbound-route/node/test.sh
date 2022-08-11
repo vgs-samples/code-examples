96273221d62d79bd9acedf031916b4d8e2e40448
Dima Marynych <dima.marynych@verygood.systems>
2022-08-11 15:19:37 +0100

    small fixes

--- vault/inbound-route/node/test.sh
+++ vault/inbound-route/node/test.sh
@@ -1,10 +1,13 @@
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 
+# Read ENV variables from .env file 
 [ ! -f .env ] || export $(grep -v '^#' .env | xargs)
 
+# Install dependencies
 npm install > '/dev/null' 2>&1
 
+# Run test
 correct_response=$(cat ./response.json | jq)
 http_response=$(node inbound.js | jq .json)
 
