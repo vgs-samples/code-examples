7e932f903b1d72724bc11b3985adf5e53a8ddd87
Anton Husak <anton.husak@verygoodsecurity.com>
2022-09-12 17:25:00 +0300

    Partial revert of PR #30

--- integrations/larky/crypto/AES_CTR/python_sample.py
+++ integrations/larky/crypto/AES_CTR/python_sample.py
@@ -7,9 +7,9 @@ from Crypto.Util import Counter
 import binascii
 from base64 import b64encode
 import string
-API_SECRET = '...'
+API_SECRET = '5243A220F218587596BC3A9E3C47A4E7B4FF98F7136856F174940D22071A35DD'
 query = 'MID=4111111111111111&exp_year=2030&exp_month=09&cvv=123&name=CoreyTaylor&amount=100'
-secondKey = '...='
+secondKey = 'MDIzM1OCNkMzRDTJGNDIc0MM1MkEz0NTA0E3Rjk5NDc='
 
 """hashpart"""
 secret = API_SECRET.encode()
@@ -19,7 +19,7 @@ hashValue = hmac.new(secret,bytes(query_tohash, encoding='utf-8'),digestmod=hash
 """Aes CTR encryption part"""
 to_encrypt = query + '&alg=SHA256&hashed_query=' + hashValue.hexdigest()
 secondKey = base64.b64decode(secondKey)
-ivv = '...' 
+ivv = 'seBheNNYzEURuyQR' 
 h = binascii.hexlify(bytes(ivv,encoding='utf-8'))
 ctr = Counter.new(128, initial_value=int(h, 16))
 chiper = AES.new(secondKey, AES.MODE_CTR, counter=ctr)
